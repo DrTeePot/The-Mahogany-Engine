b11cd8a33a127d3eba5a9ab1a43761447a1a8eaf
TeslaSmith13 <aidan.mahler13@gmail.com>
2013-05-28 13:27:16 -0400

    small changes to renderer

---
+++ src/culminating_engine/Camera.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package culminating_engine;
+
+import java.lang.Object;
+
+/**
+ *
+ * @author Aidan
+ */
+public class Camera extends GameObject {
+    
+    Angle fov = new Angle();
+    
+    Camera(Vector3[][] p, ){
+        super (p);
+    }
+    
+}
