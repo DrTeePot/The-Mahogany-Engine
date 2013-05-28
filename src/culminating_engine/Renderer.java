092e460bde64f56f21556264af1d746e75da6e77
TeslaSmith13 <aidan.mahler13@gmail.com>
2013-05-28 13:26:02 -0400

    renderer update (minor)

--- src/culminating_engine/Renderer.java
+++ src/culminating_engine/Renderer.java
@@ -4,10 +4,32 @@
  */
 package culminating_engine;
 
+import java.awt.Point;
+
+
+
 /**
  *
  * @author Aidan
  */
 public class Renderer {
+    
+    private GameObject[][] gameObjects;
+    private Camera camera;
+    
+    Renderer (GameObject[][] gOs, Camera c){
+        gameObjects = gOs;
+        camera = c;
+    }
+    
+    public Point renderPoint(Vector3 v){
+        Point p = new Point();
+        
+        
+        
+        return p;
+    }
+    
+    
     
 }
