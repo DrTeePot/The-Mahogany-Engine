80d926de11371eaf48e6a40be0019c3e7beabb14
TeslaSmith13 <aidan.mahler13@gmail.com>
2013-06-07 13:04:40 -0400

    updates.

--- src/culminating_engine/Culminating_Engine.java
+++ src/culminating_engine/Culminating_Engine.java
@@ -5,10 +5,9 @@
 package culminating_engine;
 
 import culminating_engine.shapes.GameObject;
-import culminating_engine.shapes.Cube;
+import culminating_engine.shapes.RectangularPrism;
 import culminating_engine.shapes.Face;
 import java.awt.image.BufferedImage;
-import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
@@ -25,39 +24,18 @@ public class Culminating_Engine {
      */
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
-//        GameObject si = new GameObject(new Face[] { 
+//        GameObject li = new GameObject(new Face[] { 
 //            new Face(new Vector3(0,1,1), new Vector3(0,2,1), new Vector3(0,1,2)),
 //            new Face(new Vector3(0,1,1), new Vector3(1,1,1), new Vector3(0,1,2)),
 //            new Face(new Vector3(0,1,1), new Vector3(1,1,1), new Vector3(0,2,1)),
 //            new Face(new Vector3(1,1,1), new Vector3(0,2,1), new Vector3(0,1,2))
 //        }, new Vector3(0,1,1));
-//        GameObject si = new Cube(new Vector3(1,1,1), 10);
-//        log(si.toString());
-//        si.rotateAroundWorld(PI/2,0,0);
-//        log(si.toString());
         
-        GameObject si = new GameObject(new Face[] { 
-            new Face(new Vector3(0,1,1), new Vector3(0,2,1), new Vector3(0,1,2)),
-            new Face(new Vector3(0,1,1), new Vector3(1,1,1), new Vector3(0,1,2)),
-            new Face(new Vector3(0,1,1), new Vector3(1,1,1), new Vector3(0,2,1)),
-            new Face(new Vector3(1,1,1), new Vector3(0,2,1), new Vector3(0,1,2))
-        }, new Vector3(0,1,1));
+        GameObject si = new RectangularPrism(new Vector3(0,0,0), 2,2,2);
+        si.rotateAroundSelf(PI/3, PI/2, PI/3.2);
+        GameObject[] world = {si};
         
-        GameObject di = new GameObject(new Face[] { 
-            new Face(new Vector3(0,-1,-1), new Vector3(0,-2,-1), new Vector3(0,-1,-2)),
-            new Face(new Vector3(0,-1,-1), new Vector3(-1,-1,-1), new Vector3(0,-1,-2)),
-            new Face(new Vector3(0,-1,-1), new Vector3(-1,-1,-1), new Vector3(0,-2,-1)),
-            new Face(new Vector3(-1,-1,-1), new Vector3(0,-2,-1), new Vector3(0,-1,-2))
-        }, new Vector3(0,1,1));
-        
-        GameObject center = new GameObject(new Face[] {
-            new Face(new Vector3(0,0,0), new Vector3(0,0,0), new Vector3(0,0,0))
-        }, new Vector3(0,1,1));
-        
-        GameObject[] world = {si, di, center};
-        
-        Vector3 v = new Vector3(10,7.07,7.07);
-        Camera c = new Camera(new Vector3(-10,-9,0), Math.toRadians(35), new Vector3(1,1,0));
+        Camera c = new Camera(new Vector3(-5,0,0), Math.toRadians(35), new Vector3(1,0,0));
         Renderer r = new Renderer(world, c, 600);
         
         BufferedImage b = (BufferedImage)r.wireFrameRender();
