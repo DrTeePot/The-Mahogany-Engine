23c124ffb11fc993778fbbfa5cac9574c7ef67c1
TeslaSmith13 <aidan.mahler13@gmail.com>
2013-06-10 23:00:47 -0400

    Showcases the wireFrameRenderer's new capabilities.

--- src/culminating_engine/Culminating_Engine.java
+++ src/culminating_engine/Culminating_Engine.java
@@ -7,6 +7,7 @@ package culminating_engine;
 import culminating_engine.shapes.GameObject;
 import culminating_engine.shapes.RectangularPrism;
 import culminating_engine.shapes.Face;
+import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -24,18 +25,22 @@ public class Culminating_Engine {
      */
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
-//        GameObject li = new GameObject(new Face[] { 
+//        GameObject si = new GameObject(new Face[] { 
 //            new Face(new Vector3(0,1,1), new Vector3(0,2,1), new Vector3(0,1,2)),
 //            new Face(new Vector3(0,1,1), new Vector3(1,1,1), new Vector3(0,1,2)),
 //            new Face(new Vector3(0,1,1), new Vector3(1,1,1), new Vector3(0,2,1)),
 //            new Face(new Vector3(1,1,1), new Vector3(0,2,1), new Vector3(0,1,2))
 //        }, new Vector3(0,1,1));
         
-        GameObject si = new RectangularPrism(new Vector3(10,10,10), 1,1,1);
-        si.rotateAroundSelf(PI, PI, PI);
-        GameObject[] world = {si};
+        GameObject box = new RectangularPrism(new Vector3(10,0,0), 5,7,6);
+        GameObject tri = new GameObject(new Face[] {
+             new Face(new Vector3(5,0,0), new Vector3(6,0,0),new Vector3(0,5,0))
+             }, new Vector3(0,0,0));
         
-        Camera c = new Camera(new Vector3(0,0,0), Math.toRadians(35), new Vector3(1,1,1));
+        box.rotateAroundSelf(PI/7, PI/1.4, PI/2.3);
+        GameObject[] world = {box};
+        
+        Camera c = new Camera(new Vector3(0,0,0), Math.toRadians(30), new Vector3(1,0,0));
         Renderer r = new Renderer(world, c, 600);
         
         BufferedImage b = (BufferedImage)r.wireFrameRender();
