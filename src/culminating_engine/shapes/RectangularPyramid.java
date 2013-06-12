913c76d01e0ae2710a4763b8e52968548d49f0d0
DrTeePot <tristanpotter@rogers.com>
2013-06-12 12:51:55 -0400

    Triangular Pyramid

---
+++ src/culminating_engine/shapes/RectangularPyramid.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package culminating_engine.shapes;
+
+import culminating_engine.Vector3;
+
+/**
+ *
+ * @author tristan
+ */
+public class RectangularPyramid extends GameObject{
+    private double length;
+    private double width;
+    private double height;
+    private double sideLength;
+    
+    RectangularPyramid(Vector3 origin, double length, double width, double height){
+        
+    }
+}
