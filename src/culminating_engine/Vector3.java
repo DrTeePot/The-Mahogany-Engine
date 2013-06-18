d5653a245abab86dd8134d00895767fdb0f7007c
DrTeePot <tristanpotter@rogers.com>
2013-06-18 14:24:23 -0400

    COMMENTS

--- src/culminating_engine/Vector3.java
+++ src/culminating_engine/Vector3.java
@@ -10,9 +10,9 @@ package culminating_engine;
  */
 public class Vector3 {
     
-    private double x;
-    private double y;
-    private double z;
+    private double x; //the magnitude of the x component of the vector
+    private double y; //the magnitude of the y component of the vector
+    private double z; //the magnitude of the z component of the vector
     
     /**
      * Create a Vector3 object that stretches from the origin to point (i, j, k). This is a position vector. <br>
@@ -208,57 +208,92 @@ public class Vector3 {
         return(Math.sqrt((x*x + y*y + z*z)));
     }
     
+    /**
+     * Returns the angle between this vector and a given vector in radians. <br>
+     * pre: none <br>
+     * post: a double representing the angle between this vector and a given vector is returned.
+     * @param s - the Vector3 object to compare the orientations to.
+     * @return the angle between the two Vector3 objects in radians.
+     */
     public double getAngle(Vector3 s){
         return(Math.acos(this.dotMultiply(s) / (this.getMagnitude() * s.getMagnitude() ) ) );
     }
     
+    /**
+     * Rotates a vector by a, b, c radians around the x,y,z axis of the world. <br>
+     * pre: none <br>
+     * post: The vector is modified to have a new orientation.
+     * @param a - the amount to rotate around the worlds x axis.
+     * @param b - the amount to rotate around the worlds y axis.
+     * @param c - the amount to rotate around the worlds z axis.
+     */
     public void rotate(double a, double b, double c){
-        //double xa = x;
-        //double ya = Math.cos(a)*y + Math.sin(a) * z;
-        //double za = (-1)*Math.sin(a) * y + Math.cos(a) * z;
-        //double xb = (Math.cos(b) * x - Math.sin(b) * ((-1)*Math.sin(a) * y + Math.cos(a) * z));
-        //double yb = (Math.cos(a)*y + Math.sin(a) * z);
-        //double zb = (Math.sin(b) * x + Math.cos(b) * ((-1)*Math.sin(a) * y + Math.cos(a) * z));
-        double xc = Math.cos(c) * (Math.cos(b) * x - Math.sin(b) * ((-1)*Math.sin(a) * y + Math.cos(a) * z)) + Math.sin(c) * (Math.cos(a)*y + Math.sin(a) * z);
-        double yc = (-1) * Math.sin(c) * (Math.cos(b) * x - Math.sin(b) * ((-1)*Math.sin(a) * y + Math.cos(a) * z))  + Math.cos(c)*(Math.cos(a)*y + Math.sin(a) * z);
-        double zc = (Math.sin(b) * x + Math.cos(b) * ((-1)*Math.sin(a) * y + Math.cos(a) * z));
+        double cosa = Math.cos(a);
+        double cosb = Math.cos(b);
+        double cosc = Math.cos(c);
+        double sina = Math.sin(a);
+        double sinb = Math.sin(b);
+        double sinc = Math.sin(c);
+                
+        double xc = cosc * (cosb * x - sinb * ((-1) * sina * y + cosa * z)) + sinc * (cosa * y + sina * z);
+        double yc = (-1) * sinc * (cosb * x - sinb * ( (-1) * sina * y + cosa * z) )  + cosc * (cosa * y + sina * z);
+        double zc = (sinb * x + cosb * ((-1) * sina * y + cosa * z));
         
         this.setVector(xc, yc, zc);
     }
     
+    /**
+     * Rotates the vector towards a given vector by r radians. Note that if this vector and the given vector are
+     * co-linear (parallel) then this will return a vector comprised of components that are "Not a Number". <br>
+     * pre: none <br>
+     * post: the vector is modified to have a new orientation. 
+     * @param v - the Vector3 object to rotate towards.
+     * @param r - the amount in radians to rotate around v.
+     */
     public void rotateTowards(Vector3 v, double r){
+        //find the vector to rotate around by cross multiplication
         Vector3 n = new Vector3(Vector3.crossMultiply(this, v)); //vector perpendicular to both this and v
         
+        //rotate around the cross product vector 
         this.rotateAround(n, r);
     }
     
+    /**
+     * Rotates the vector around a given vector by r radians. <br>
+     * pre: none <br>
+     * post: the vector is modified to have a new orientation. 
+     * @param v - the Vector3 object to rotate around.
+     * @param r - the amount in radians to rotate around v.
+     */
     public void rotateAround(Vector3 v, double d){
-        //System.out.println(this.getMagnitude());
+        
+        //save magnitudes, rotation for unit vectors
         double magThis = this.getMagnitude();
         double magV = v.getMagnitude();
         
+        // get unit vector components for vector to rotate around
         double vx = v.getComponents()[0]/magV;
         double vy = v.getComponents()[1]/magV;
         double vz = v.getComponents()[2]/magV;
-        
-        
-        //System.out.println(vx + " " + vy + " " + vz);
-        //System.out.println(Vector3.getMagnitude(vx, vy, vz));
-                 
-        
+            
+        // get unit vector components for vector to rotate
         double x1 = x / magThis;
         double y1 = y / magThis;
         double z1 = z / magThis;
         
+        //minimize trig calls
         double cos = Math.cos(d);
         double sin = Math.sin(d);
         
+        // minimize dot calls, simply rotation matrix code
         double dot = Vector3.dotMultiply(x1,y1,z1, vx, vy, vz);
         
+        //the rotation matrix for unit vectors
         double rx = vx * dot * (1-cos) + x1 * cos + ( (vy * z1) - (vz * y1) ) * sin;
         double ry = vy * dot * (1-cos) + y1 * cos + ( (vz * x1) - (vx * z1) ) * sin;
         double rz = vz * dot * (1-cos) + z1 * cos + ( (vx * y1) - (vy * x1) ) * sin;
         
+        //set vector to new rotation, with correct magnitude.
         x = rx * magThis;
         y = ry * magThis;
         z = rz * magThis;
@@ -383,11 +418,26 @@ public class Vector3 {
     
     //<editor-fold desc="Static methods">
 
-    
+    /**
+     * Return the magnitude of the vector as a double. <br>
+     * pre: none <br>
+     * post: the vector's magnitude is returned.
+     * @param i - the x component of the vector to find the magnitude of.
+     * @param j - the y component of the vector to find the magnitude of.
+     * @param k - the z component of the vector to find the magnitude of.
+     * @return magVector
+     */
     public static double getMagnitude(double i, double j, double k){
         return(Math.sqrt(i*i + j*j + k*k));
     }
     
+    /**
+     * Return the magnitude of the vector as a double. <br>
+     * pre: none <br>
+     * post: the vector's magnitude is returned.
+     * @param 
+     * @return magVector
+     */
     public static double getMagnitude(Vector3 s){
         double i = s.getComponents()[0];
         double j = s.getComponents()[1];
@@ -395,6 +445,14 @@ public class Vector3 {
         return(Math.sqrt(i*i + j*j + k*k));
     }
     
+    /**
+     * Return the dot product of the vector by another vector as a double <br>
+     * pre: none <br>
+     * post: The dot product is returned
+     * @param s - the first Vector3 object to dot multiply.
+     * @param d - the second Vector3 object to dot multiply.
+     * @return s (dot) d
+     */
     public static double dotMultiply(Vector3 s, Vector3 d){
         double x1 = s.getComponents()[0];
         double y1 = s.getComponents()[1];
@@ -407,6 +465,18 @@ public class Vector3 {
         return(x1*x2 + y1*y2 + z1*z2);
     }
     
+    /**
+     * Return the dot product of the vector by another vector as a double <br>
+     * pre: none <br>
+     * post: The dot product is returned
+     * @param x1 - the magnitude of the x component to be dotted.
+     * @param y1 - the magnitude of the y component to be dotted.
+     * @param z1 - the magnitude of the z component to be dotted.
+     * @param x2 - the magnitude of the x component to be dotted.
+     * @param y2 - the magnitude of the y component to be dotted.
+     * @param z2 - the magnitude of the z component to be dotted.
+     * @return Vector3(x1,y1,z1) (dot) Vector3(x2,y2,z2)
+     */
     public static double dotMultiply(double x1, double y1, double z1, double x2, double y2, double z2){
         return(x1*x2 + y1*y2 + z1*z2);
     }
@@ -472,6 +542,7 @@ public class Vector3 {
      * @param a - the value by which to multiply the x component
      * @param b - the value by which to multiply the y component
      * @param c - the value by which to multiply the z component
+     * @param v - the vector to multiply by the matrix [a/b/c]
      * @return the Vector3 object (ax, by, cz)
      */
     public static Vector3 matrixMultiply(double a, double b, double c, Vector3 v){
@@ -481,10 +552,26 @@ public class Vector3 {
         return(new Vector3(a*x,b*y,c*z));
     }
     
+    /**
+     * Returns the angle between a vector and another vector in radians. <br>
+     * pre: none <br>
+     * post: a double representing the angle between a vector and another vector is returned.
+     * @param u - the first Vector3 object to compare the orientation. 
+     * @param s - the second Vector3 object to compare the orientation.
+     * @return the angle between the two Vector3 objects in radians.
+     */
     public static double getAngle(Vector3 u, Vector3 v){
         return(Math.acos(Vector3.dotMultiply(u,v) / (u.getMagnitude() * v.getMagnitude() ) ) );
     }
     
+    /**
+     * Add two vectors. <br>
+     * pre: none <br>
+     * post: A vector is returned that is (vector s) + (vector d).
+     * @param s - first vector to add.
+     * @param d - the second vector to add.
+     * @return Vector3 (s + d)
+     */
     public static Vector3 addVectors(Vector3 s, Vector3 d){
         double x1 = s.getComponents()[0];
         double y1 = s.getComponents()[1];
@@ -496,6 +583,14 @@ public class Vector3 {
         return(new Vector3(x1 + x2, y1+y2, z1+z2));
     }
     
+    /**
+     * Subtract two vectors. <br>
+     * pre: none <br>
+     * post: The vector is altered to be (vector s) - (vector d).
+     * @param s - the first vector to subtract.
+     * @param d - the second vector to subtract.
+     * @return Vector3(s-d)
+     */
     public static Vector3 subtractVectors(Vector3 s, Vector3 d){
         double x1 = s.getComponents()[0];
         double y1 = s.getComponents()[1];
@@ -507,8 +602,48 @@ public class Vector3 {
         return(new Vector3(x2 - x1, y2-y1, z2-z1));
     }
     
+    /**
+     * Returns the vector that results from projecting a vector on another vector.
+     * pre: none
+     * post: Returns the vector in the direction of s with a magnitude of the s component of the vector.
+     * @param t - the vector being projected.
+     * @param s - the vector being projected on.
+     * @return the projection vector
+     */
+    public static Vector3 projectionOn(Vector3 t, Vector3 s){
+        double k = (t.dotMultiply(s)/(s.getMagnitude() * s.getMagnitude()));
+        Vector3 v = new Vector3(Vector3.scalarMultiply(k,s));
+        return(v);
+    }
+    
+    /**
+     * Returns the vector that results from projecting a vector on another vector. <br>
+     * pre: none <br>
+     * post: Returns the vector in the direction of (i,j,k) with a magnitude of the first vector.
+     * @param x - the magnitude of the x component of the vector being projected.
+     * @param y - the magnitude of the y component of the vector being projected.
+     * @param z - the magnitude of the z component of the vector being projected.
+     * @param i - the magnitude of the x component of the vector being projected on.
+     * @param j - the magnitude of the y component of the vector being projected on.
+     * @param k - the magnitude of the z component of the vector being projected on.
+     * @return the projection vector
+     */
+    public static Vector3 projectionOn(double x, double y, double z, double i, double j, double k){
+        Vector3 t = new Vector3(x,y,z);
+        double c = (t.dotMultiply(i,j,k)/(i*i + j*j + k*k));
+        Vector3 v = new Vector3(c*i, c*j, c*k);
+        return(v);
+    }
+            
     //</editor-fold>
     
+    /**
+     * Returns the string of the object. This is created for debugging purposes, and displays a textual version of the 
+     * object. <br>
+     * pre: none
+     * post: All of the components in the Vector3 are returned in an organized list.
+     * @return A string of all the components, organized using brackets in algebraic form is returned to the client.
+     */
     @Override
     public String toString(){
         return("( " + x + ", " + y + ", " + z + " )");
